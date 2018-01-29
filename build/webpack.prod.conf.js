c37b87e5e8d60255dda44803639ea3f36ddb0126
Josantonius <hello@josantonius.com>
2018-01-29 18:27:10 +0100

    First version

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -29,7 +29,10 @@ var webpackConfig = merge(baseWebpackConfig, {
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        BASE_URL: "'http://localhost:3000'"
+      }
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
